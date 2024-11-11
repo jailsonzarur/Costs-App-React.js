e80d42d3cf0bb4ce1c6b85098ed51467c3087a82
Jailson Zarur <jailsoncardoz123@gmail.com>
2024-11-11 13:17:41 -0300

    Estruturando o layout do conatiner e dos Routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <>
-      <h1>Costs</h1>
-    </>
+    <Router future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Container customClass="min_height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/company' element={<Company />}></Route>
+          <Route exact path='/contact' element={<Contact />}></Route>
+          <Route exact path='/newproject' element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
