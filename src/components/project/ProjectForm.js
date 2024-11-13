50bc239d91043dcfb61f344652879ca280b1a15f
Jailson Zarur <jailsoncardoz123@gmail.com>
2024-11-13 15:38:49 -0300

    Componetização do Formulário e criação do json-server(simulador de uma api backend).

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,17 @@
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 import style from './ProjectForm.module.css'
 
 function ProjectForm() {
     return (
-        <form className={style.criar_projeto}>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto."></input>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total."></input>
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled selected>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type='submit' value="Criar projeto"></input>
-            </div>
+        <form className={style.form}>
+            <Input type="text" text="Nome do projeto" name="Nome do projeto"
+            placeholder="Insira o nome do projeto."/>
+            <Input type="number" text="Orçamento do projeto" name="Orçamento do projeto"
+            placeholder="Insira o orçamento total."/>
+            <Select text="Selecione uma categoria" name="category_id"/>
+            <SubmitButton text="Criar"/>
         </form>
     )
 }
